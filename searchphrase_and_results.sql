1a4d5924945653791478a10312536854f26fbc88
Loke Sonne <ls@sonnenielsen.dk>
2017-04-26 20:17:53 +0200

    Joined on user table
    Also moved the substring functions to the temp tables

--- searchphrase_and_results.sql
+++ searchphrase_and_results.sql
@@ -1,56 +1,70 @@
-
-select * from #searchphrase
-select * from #numberofresults
-
 drop table #searchphrase
 drop table #numberofresults
 
-select *
+select message,timestamp,u.id as userid
+	,case when PATINDEX ( '%CLIENT: Identifier=%' , message ) = 0 then 'no user'
+		else
+		substring(message,
+		PATINDEX ( '%CLIENT: Identifier%' , message )+20,
+		PATINDEX ( '%, Organization=%' , message )-(PATINDEX ( '%CLIENT: Identifier%' , message )+21)
+		) 
+		end as identifier
+	,case when PATINDEX ( '%PARAMETERS: <string>%' , message ) = 0 then 'no searchphrase'
+		else
+		substring(message,
+		PATINDEX ( '%PARAMETERS: <string>%' , message )+20,
+		PATINDEX ( '%</string>%' , message )-(PATINDEX ( '%PARAMETERS: <string>%' , message )+20)
+		) 
+		end as searchphrase		
 into #searchphrase
 from logs
+inner join [user] u on u.identifier =  
+		substring(message,
+		PATINDEX ( '%CLIENT: Identifier%' , message )+20,
+		PATINDEX ( '%, Organization=%' , message )-(PATINDEX ( '%CLIENT: Identifier%' , message )+21)
+		) 
 where 
-(
-	(partitionkey = 'SonneNielsen.Server.WebApplication.Container' and message like '%ls@sonnenielsen.dk%' and message like '%SonneNielsen.Server.WebApplication.Controllers.CompaniesController.Search%')
-) 
+partitionkey = 'SonneNielsen.Server.WebApplication.Container' 
+and message like '%SonneNielsen.Server.WebApplication.Controllers.CompaniesController.Search%'
+and PATINDEX ( '%CLIENT: Identifier=%' , message ) > 0
+and PATINDEX ( '%PARAMETERS: <string>%' , message ) > 0
 and datepart(year,timestamp) = 2017
 and datepart(month,timestamp) = 04
-order by timestamp
 
 
-select * 
+select message,timestamp
+	,case when PATINDEX ( '%UserId[:]%' , message ) = 0 then 'no user'
+		else
+		substring(message,
+		PATINDEX ( '%UserId[:]%' , message )+7,
+		PATINDEX ( '%.%' , message )-(PATINDEX ( '%UserId[:]%' , message )+8)
+		) 
+		end as userid
+	,case when PATINDEX ( '%SearchCompanies found %' ,message ) = 0 then 'no searchphrase'
+		else
+		substring(message,
+		PATINDEX ( '%SearchCompanies found %' ,message )+22,
+		PATINDEX ( '%results%' ,message )-(PATINDEX ( '%SearchCompanies found %' ,message )+22)
+		) 
+		end as numberOfResults		
 into #numberofresults
 from logs
 where 
-(
-	(partitionkey = 'SonneNielsen.Server.Business.Container' and message like '%UserId:1004%' and message like 'SearchCompanies found%')
-) 
+partitionkey = 'SonneNielsen.Server.Business.Container' 
+and message like 'SearchCompanies found%'
+and PATINDEX ( '%UserId[:]%' , message ) > 0
 and datepart(year,timestamp) = 2017
 and datepart(month,timestamp) = 04
-order by timestamp
 
 select s.timestamp
-	,case when PATINDEX ( '%PARAMETERS: <string>%' , s.message ) = 0 then 'no searchphrase'
-		else
-		substring(s.message,
-		PATINDEX ( '%PARAMETERS: <string>%' , s.message )+20,
-		PATINDEX ( '%</string><double>%' , s.message )-(PATINDEX ( '%PARAMETERS: <string>%' , s.message )+20)
-		) 
-		end as searchphrase
-	,case when PATINDEX ( '%SearchCompanies found %' , nextresult.message ) = 0 then 'no searchphrase'
-		else
-		substring(nextresult.message,
-		PATINDEX ( '%SearchCompanies found %' , nextresult.message )+22,
-		PATINDEX ( '%results%' , nextresult.message )-(PATINDEX ( '%SearchCompanies found %' , nextresult.message )+22)
-		) 
-		end as numberOfResults	
-		
+		,nextresult.numberOfResults
+		,s.userid	
 from #searchphrase s
-	join [user] u on u.
 outer apply
 	(
-	select top 1 message,timestamp
+	select top 1 message,timestamp,numberOfResults
 	from #numberofresults
-	where timestamp > s.timestamp
+	where timestamp > s.timestamp and userid = cast(s.userid as int)
 	order by timestamp
 	) nextresult
-where PATINDEX ( '%PARAMETERS: <string>%' , s.message ) > 0
+order by userid,timestamp
